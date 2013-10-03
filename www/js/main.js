296fdadfb79928684ea7c4f1b397df9f6f5396ae
Andrew Donald <andrew.a.donald@gmail.com>
2013-10-02 23:06:44 -0400

    css3 transitions for client header

--- www/js/main.js
+++ www/js/main.js
@@ -2,3 +2,9 @@ function startApp(){
     $('.app').addClass('hide');
     $('.page.home').removeClass('hide');
 }
+
+$('#stream-header .status').on('click',function(){
+    $(this).children('.toggle').toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+    $(this).next().slideToggle('fast');
+    $('body').toggleClass('client-header-expanded');
+});
