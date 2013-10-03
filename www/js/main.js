92ab652ccfe84979c67f4486211fd7d828f6d482
Andrew Donald <andrew.a.donald@gmail.com>
2013-10-03 08:08:54 -0400

    Header and Footer Stream and Filter controls

--- www/js/main.js
+++ www/js/main.js
@@ -8,3 +8,35 @@ $('#stream-header .status').on('click',function(){
     $(this).next().slideToggle('fast');
     $('body').toggleClass('client-header-expanded');
 });
+
+
+$('#footer .status').on('click',function(){
+    $('body').toggleClass('stream-activity-filters-expanded');
+    $(this)
+        .children('.toggle').toggleClass('glyphicon-chevron-down glyphicon-chevron-up')
+        .siblings('.stream-activity-filters').slideToggle('fast');
+});
+
+$('.stream-activity-filters button').on('click',function(){
+    $(this).toggleClass('btn-warning btn-success');
+    if($(this).is('.activity-all.btn-warning')){ // If Show All Activities is deselected
+        $('.stream-activity-filters-media .btn').removeClass('btn-success').addClass('btn-warning');
+        $('.stream-activity-filters-media .btn.activity-people').addClass('btn-success');
+        $('.stream-activity-filters-media').removeClass('all-selected');
+    }else if($(this).is('.activity-all.btn-success')){ // If Show All Activities is selected
+        $('.stream-activity-filters-media .btn').removeClass('btn-warning').addClass('btn-success');
+        $('.stream-activity-filters-media').addClass('all-selected');
+    }else if($(this).parent('.stream-activity-filters-media').size() > 0){ // If any single activity is selected
+        $('.stream-activity-filters .btn.activity-all').removeClass('btn-success').addClass('btn-warning');
+        $('.stream-activity-filters-media').removeClass('all-selected');
+    }
+
+    if($('.stream-activity-filters-media .btn.btn-warning').size() == 0){ // If all activity buttons are selected
+        $('.stream-activity-filters-media').addClass('all-selected');
+        $('.stream-activity-filters .btn.activity-all').removeClass('btn-warning').addClass('btn-success');
+    }
+
+    if($('.stream-activity-filters .btn.btn-success').size() == 0){ // If No activity buttons are selected
+        $('.stream-activity-filters-media .btn.activity-people').removeClass('btn-warning').addClass('btn-success');
+    }
+});
