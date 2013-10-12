246fc813b12632c8ef5c103e62e7ba8565b4372b
Andrew Donald <andrew.a.donald@gmail.com>
2013-10-12 13:29:33 -0400

    UI fix

--- www/js/main.js
+++ www/js/main.js
@@ -86,6 +86,12 @@ $(".stream-pane-menu-con").animate({
 
 //* EVENT HANDLERS *//////////////////////////////////////////////////////////////
 
+// Search Toggle
+$('#search-toggle').on('click',function(){
+    $('.bottom-panel').slideToggle();
+});
+$('#search-toggle').click();
+
 // Main Menu Toggle
 $('#main-menu-toggle').on('click',function(){
     $('#main-menu-con').toggleClass('active');
